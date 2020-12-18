9202f0fc95bd5fc9e1b5164e0ae8fda5875a9b8f
omid-joukar <joukaromid6@gmail.com>
2020-12-18 10:23:22 +0100

    adding Enviroment Property.

--- src/main/java/omid/springframework/config/PropertyConfig.java
+++ src/main/java/omid/springframework/config/PropertyConfig.java
@@ -1,15 +1,19 @@
 package omid.springframework.config;
 
 import omid.springframework.examplebeans.FakeDatasource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
+    @Autowired
+    Environment environment;
     @Value("${omid.username}")
     String user;
     @Value(("${omid.password}"))
@@ -19,7 +23,7 @@ public class PropertyConfig {
     @Bean
     public FakeDatasource fakeDatasource(){
         FakeDatasource fakeDatasource = new FakeDatasource();
-        fakeDatasource.setUser(user);
+        fakeDatasource.setUser(environment.getProperty("USERNAME"));
         fakeDatasource.setPassword(password);
         fakeDatasource.setUrl(url);
         return fakeDatasource;
