9679fccf63ec2babfbe12d70d946fea184339bd9
omid-joukar <joukaromid6@gmail.com>
2020-12-18 11:12:26 +0100

    adding Properties to application.properties.

--- src/main/java/omid/springframework/config/PropertyConfig.java
+++ src/main/java/omid/springframework/config/PropertyConfig.java
@@ -2,22 +2,16 @@ package omid.springframework.config;
 
 import omid.springframework.examplebeans.FakeDatasource;
 import omid.springframework.examplebeans.FakeJmsBroker;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
+
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.env.Environment;
+
 
 @Configuration
-@PropertySources({
-        @PropertySource("classpath:datasource.properties"),
-        @PropertySource("classpath:jms.properties")})
 public class PropertyConfig {
-    @Autowired
-    Environment environment;
+
     @Value("${omid.username}")
     String user;
     @Value(("${omid.password}"))
@@ -30,12 +24,11 @@ public class PropertyConfig {
     String jmsPassword;
     @Value("${omid.jms.dburl}")
     String jmsUrl;
-    @Value("${omid.jms.class}")
-    String jmsClass;
+
     @Bean
     public FakeDatasource fakeDatasource(){
         FakeDatasource fakeDatasource = new FakeDatasource();
-        fakeDatasource.setUser(environment.getProperty("USERNAME"));
+        fakeDatasource.setUser(user);
         fakeDatasource.setPassword(password);
         fakeDatasource.setUrl(url);
         return fakeDatasource;
@@ -46,7 +39,6 @@ public class PropertyConfig {
         fakeJmsBroker.setUserName(jmsUser);
         fakeJmsBroker.setPassword(password);
         fakeJmsBroker.setUrl(jmsUrl);
-        fakeJmsBroker.setClasses(jmsClass);
         return fakeJmsBroker;
     }
     @Bean
--- src/main/java/omid/springframework/examplebeans/FakeJmsBroker.java
+++ src/main/java/omid/springframework/examplebeans/FakeJmsBroker.java
@@ -4,15 +4,7 @@ public class FakeJmsBroker {
     private String userName;
     private String password;
     private String url;
-    private String classes;
 
-    public String getClasses() {
-        return classes;
-    }
-
-    public void setClasses(String classes) {
-        this.classes = classes;
-    }
 
     public String getUserName() {
         return userName;
